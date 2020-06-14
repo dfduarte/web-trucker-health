9aaa16122bbe42fa325167aacdd66fbf482fe665
GabrieleSuzart <gabrielesuzart@gmail.com>
2020-06-14 03:51:30 -0300

    Updates

--- src/design-system/baseProps.ts
+++
@@ -1,5 +0,0 @@
-import { AllHTMLAttributes, ComponentType } from 'react';
-
-export type As = keyof JSX.IntrinsicElements | ComponentType;
-export type ForwardedAs = keyof JSX.IntrinsicElements | ComponentType;
-export type HtmlAttrs = AllHTMLAttributes<HTMLElement> | {};
